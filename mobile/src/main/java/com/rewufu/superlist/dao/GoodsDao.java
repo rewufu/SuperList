5e671d9079d789740d24a6f48dd8ebd0455a3988
Bell <617573470@qq.com>
2015-08-11 19:29:57 +0800

    add goods.json in assets, init goods table when database create

---
+++ mobile/src/main/java/com/rewufu/superlist/dao/GoodsDao.java
@@ -0,0 +1,44 @@
+package com.rewufu.superlist.dao;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Bell on 7/22/15.
+ */
+public class GoodsDao {
+    private SQLiteOpenHelper openHelper;
+    private Context context;
+
+    public GoodsDao(Context context) {
+        this.context = context;
+        openHelper = new com.rewufu.superlist.database.SQLiteOpenHelper(context);
+    }
+
+    //query goods
+    public ArrayList<String> queryGoods() {
+        SQLiteDatabase db = openHelper.getReadableDatabase();
+        if (db.isOpen()) {
+            Cursor cursor = db.rawQuery("select gName from goods;", null);
+            if (cursor != null && cursor.getCount() > 0) {
+                ArrayList<String> lists = new ArrayList<>();
+                while (cursor.moveToNext()) {
+                    String list = cursor.getString(0);
+                    lists.add(list);
+                }
+                db.close();
+                return lists;
+            }
+            db.close();
+            return null;
+        }
+        return null;
+    }
+
+
+
+}
--- mobile/src/main/java/com/rewufu/superlist/database/SQLiteOpenHelper.java
+++ mobile/src/main/java/com/rewufu/superlist/database/SQLiteOpenHelper.java
@@ -2,17 +2,28 @@ package com.rewufu.superlist.database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 /**
  * Created by Bell on 7/22/15.
  */
 public class SQLiteOpenHelper extends android.database.sqlite.SQLiteOpenHelper {
-
+    private Context context;
     public static final  String CREATE_GOODS = "create table goods (gName varchar(20) primary key, kind varchar(20))";
     public static final  String CREATE_LISTS = "create table lists (list varchar(20))";
 
     public SQLiteOpenHelper(Context context) {
         super(context, "SuperList.db", null, 1);
+        this.context = context;
     }
 
     @Override
@@ -20,6 +31,25 @@ public class SQLiteOpenHelper extends android.database.sqlite.SQLiteOpenHelper {
         sqLiteDatabase.execSQL(CREATE_GOODS);
         sqLiteDatabase.execSQL(CREATE_LISTS);
         //init database
+        String json = "";
+        try {
+            InputStream inputStream = context.getAssets().open("database/goods.json");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuffer stringBuffer = new StringBuffer();
+            String str = "";
+            while ((str = reader.readLine()) != null){
+                stringBuffer.append(str);
+            }
+            json = stringBuffer.toString();
+        } catch (IOException e) {
+            Log.e("ReadJson", e.getMessage());
+        }
+        Gson gson = new Gson();
+        Map<String, String> map = gson.fromJson(json, new TypeToken<Map<String, String>>(){}.getType());
+        for(Map.Entry<String, String> entry : map.entrySet()){
+            sqLiteDatabase.execSQL("insert into goods(gName, kind) values (?, ?);",
+                    new Object[]{entry.getKey(), entry.getValue()});
+        }
     }
 
     @Override
